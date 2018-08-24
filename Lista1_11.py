eb4ba935ebcbf9f67d9ba7763227555f5ae2b5d8
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 16:01:23 -0300

    Update Lista1_11.py

--- Lista1_11.py
+++ Lista1_11.py
@@ -1,3 +1,10 @@
+'''
+Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+o produto do dobro do primeiro com metade do segundo .
+a soma do triplo do primeiro com o terceiro.
+o terceiro elevado ao cubo.
+'''
+
 inteiro1 = input("Digite um número inteiro: ")
 inteiro2 = input("Digite outro número inteiro: ")
 real = input("Digite um número real")
