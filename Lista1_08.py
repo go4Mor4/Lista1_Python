17f665c8a56d7cf993047a73f341c1aa89e10b9e
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 15:58:07 -0300

    Update Lista1_08.py

--- Lista1_08.py
+++ Lista1_08.py
@@ -1,3 +1,5 @@
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
 quant_hora = input("Quanto você ganha por hora? R: ")
 horas_mes = input("Quantas horas você trabalha no mês? R: ")
 
