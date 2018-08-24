857a4020a120f5c242ad5c8c2c931a3e1eea1efe
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 16:02:50 -0300

    Update Lista1_14.py

--- Lista1_14.py
+++ Lista1_14.py
@@ -1,3 +1,7 @@
+'''
+João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens adequadas.
+'''
+
 peso = input("Digite o peso dos peixes: ")
 execesso = int(peso) - 50
 multa = int(execesso) * 4
