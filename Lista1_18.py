d637e55315a9fa0bb29c70fa248119c59940f87a
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 16:05:16 -0300

    Update Lista1_18.py

--- Lista1_18.py
+++ Lista1_18.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+'''
+
 import math
 
 tamanho = input("Digite o tamanho do arquivo: ")
