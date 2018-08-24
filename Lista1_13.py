32067ba0cca304c63a5db899efe25959706ab121
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 16:02:15 -0300

    Update Lista1_13.py

--- Lista1_13.py
+++ Lista1_13.py
@@ -1,3 +1,9 @@
+'''
+Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+Para homens: (72.7*h) - 58
+Para mulheres: (62.1*h) - 44.7
+'''
+
 altura = input("Digite a altura: ")
 homem = (72.7 * float(altura)) - 58
 mulher = (62.1 * float(altura)) - 44.7
