2f309cfd078460049e287a87e7d451497bd290b0
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 16:48:34 -0300

    Delete Lista3_17.py

--- Lista3_17.py
+++
@@ -1,9 +0,0 @@
-numero = int(input("Digite um número: "))
-count1 = 0
-count = 1
-while count1 < numero:
-    fatorial = numero * (numero - count)
-    count = count - 1
-    count1 = count + 1
-
-print(fatorial)
