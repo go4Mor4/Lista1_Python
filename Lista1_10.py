3ac67d1f4111e121a71453cd016eb206ce610f06
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 16:00:54 -0300

    Update Lista1_10.py

--- Lista1_10.py
+++ Lista1_10.py
@@ -1,3 +1,5 @@
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+
 celsius = input("Quantidade de graus Celsius: ")
 fahrenheit = (float(celsius) * 1.8) + 32
 
