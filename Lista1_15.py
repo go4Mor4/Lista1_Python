d4b5889e4e10a0ac62183fc4b68c4a942b41f080
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 16:03:24 -0300

    Update Lista1_15.py

--- Lista1_15.py
+++ Lista1_15.py
@@ -1,3 +1,7 @@
+'''
+Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+'''
+
 quant_hora = input("Quanto você ganha por hora? R: ")
 horas_mes = input("Quantas horas você trabalha no mês? R: ")
 salario = float(quant_hora) * int(horas_mes)
