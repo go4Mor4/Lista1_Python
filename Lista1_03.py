5c516e1d899c551c7b843a273b28d31ae6facf25
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 16:05:48 -0300

    Update Lista1_03.py

--- Lista1_03.py
+++ Lista1_03.py
@@ -1,4 +1,4 @@
-#Faça um Programa que peça dois números e imprima a soma.
+#Faça um Programa que peça dois números e imprima a soma
 
 a = input("Digite o primeiro numero: ")
 b = input("Digite o segundo numero: ")
