949a78bfd8ee594db225823d8883f9365282c1f6
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:36:07 -0300

    Update Lista1_16.py

--- Lista1_16.py
+++ Lista1_16.py
@@ -1,10 +1,9 @@
-import math
-
-tamanho = input("Tamanho da área a ser pintada: ")
-litros = int(tamanho) / 3
-
-latas = int(litros) / 18
-valor = math.ceil(latas)
-preco = int(valor) * 80
+#Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
+from math import ceil
+tamanho = float(input("Tamanho da área a ser pintada: "))
+litros = tamanho / 3
+latas = litros / 18
+valor = ceil(latas)
+preco = valor * 80
 
 print("Latas: ", valor, "\nPreço: ", preco)
