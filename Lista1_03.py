cd884b9ed13120d8731c71510c4b980bad71e844
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:08:37 -0300

    Update Lista1_03.py

--- Lista1_03.py
+++ Lista1_03.py
@@ -1,4 +1,5 @@
-a = input("Digite o primeiro numero: ")
-b = input("Digite o segundo numero: ")
-soma = int(a) + int(b)
-print("A soma de", a, "+", b, "eh igual a", soma)
+# Faça um Programa que peça dois números e imprima a soma.
+a = float(input("Digite o primeiro numero: "))
+b = float(input("Digite o segundo numero: "))
+
+print("A soma de", a, "+", b, "eh igual a", a + b)
