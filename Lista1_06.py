a32c5db3dbee5b16ed177609508ffd199bc5cfd8
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:11:07 -0300

    Update Lista1_06.py

--- Lista1_06.py
+++ Lista1_06.py
@@ -1,4 +1,5 @@
-raio = input("Digite o raio do circulo: ")
-area = 3.14 * float(raio)
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio = float(input("Digite o raio do circulo: "))
+area = 3.14 * raio
 
 print("A área do circlo é de: ", area)
