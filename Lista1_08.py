f6114279e4a2382d0f5f7e2c537f3a1d5405ee11
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:13:15 -0300

    Update Lista1_08.py

--- Lista1_08.py
+++ Lista1_08.py
@@ -1,5 +1,6 @@
-quant_hora = input("Quanto você ganha por hora? R: ")
-horas_mes = input("Quantas horas você trabalha no mês? R: ")
+# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+quant_hora = float(input("Quanto você ganha por hora? R: "))
+horas_mes = int(input("Quantas horas você trabalha no mês? R: "))
 
-salario = float(quant_hora) * int(horas_mes)
+salario = quant_hora * horas_mes
 print("O seu salário no final do mês será de R$", salario)
