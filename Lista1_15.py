426540d1093782884dd641ccadf7b0634537d159
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:29:14 -0300

    Update Lista1_15.py

--- Lista1_15.py
+++ Lista1_15.py
@@ -1,10 +1,16 @@
-quant_hora = input("Quanto você ganha por hora? R: ")
-horas_mes = input("Quantas horas você trabalha no mês? R: ")
-salario = float(quant_hora) * int(horas_mes)
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+# salário bruto.
+# quanto pagou ao INSS.
+# quanto pagou ao sindicato.
+# o salário líquido.
 
-ir = float(salario) * 0.11
-inss = float(salario) * 0.08
-sindicato = float(salario) * 0.05
-salario_liquido = float(salario) - float(ir) - float(inss) - float(sindicato)
+quant_hora = float(input("Quanto você ganha por hora? R: "))
+horas_mes = int(input("Quantas horas você trabalha no mês? R: "))
+salario = quant_hora * horas_mes
+
+ir = salario * 0.11
+inss = salario * 0.08
+sindicato = salario * 0.05
+salario_liquido = salario - ir - inss - sindicato
 
 print("Salário bruto: ", salario, "\nIR: ", ir, "\nINSS: ", inss, "\nSindicato ", sindicato, "\nSalário Liquido: ", salario_liquido)
