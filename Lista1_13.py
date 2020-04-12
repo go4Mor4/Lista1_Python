75b97aabf350cdf202a6c10e5336d61b5e6e0bad
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:22:00 -0300

    Update Lista1_13.py

--- Lista1_13.py
+++ Lista1_13.py
@@ -1,5 +1,9 @@
-altura = input("Digite a altura: ")
-homem = (72.7 * float(altura)) - 58
-mulher = (62.1 * float(altura)) - 44.7
+#Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+#Para homens: (72.7*h) - 58
+#Para mulheres: (62.1*h) - 44.7
+
+altura = float(input("Digite a altura: "))
+homem = (72.7 * altura) - 58
+mulher = (62.1 * altura) - 44.7
 
 print("Homem: ", homem, "\nMulher: ", mulher)
