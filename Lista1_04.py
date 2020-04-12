2fcbc37caaa453a647fa076307ea179b72300219
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:09:56 -0300

    Update Lista1_04.py

--- Lista1_04.py
+++ Lista1_04.py
@@ -1,8 +1,9 @@
-n1 = input("Nota 1: ")
-n2 = input("Nota 2: ")
-n3 = input("Nota 3: ")
-n4 = input("Nota 4: ")
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+n3 = float(input("Nota 3: "))
+n4 = float(input("Nota 4: "))
 
-media = (float(n1) + float(n2) + float(n3) + float(n4)) / 4
+media = (n1 + n2 + n3 + n4) / 4
 
 print("A média do aluno será: ", media)
