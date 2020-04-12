423e89c721ad4bf4a79d634cd3cb9e69ebc969dc
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:18:52 -0300

    Update Lista1_11.py

--- Lista1_11.py
+++ Lista1_11.py
@@ -1,9 +1,14 @@
-inteiro1 = input("Digite um número inteiro: ")
-inteiro2 = input("Digite outro número inteiro: ")
-real = input("Digite um número real")
+# Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+# o produto do dobro do primeiro com metade do segundo .
+# a soma do triplo do primeiro com o terceiro.
+# o terceiro elevado ao cubo.
 
-a = (int(inteiro1) * 2) + (int(inteiro2) / 2)
-b = (int(inteiro1) * 3) + float(real)
-c = float(real) ** 3
+inteiro1 = int(input("Digite um número inteiro: "))
+inteiro2 = int(input("Digite outro número inteiro: "))
+real = float(input("Digite um número real"))
+
+a = (inteiro1 * 2) + (inteiro2 / 2)
+b = (inteiro1 * 3) + real
+c = real ** 3
 
 print("a : ", a, "\nb : ", b, "\nc : ", c)
