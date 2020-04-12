90649b9deda8412df141777f371388aa7cbe5792
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:23:37 -0300

    Update Lista1_14.py

--- Lista1_14.py
+++ Lista1_14.py
@@ -1,8 +1,7 @@
-peso = input("Digite o peso dos peixes: ")
-execesso = int(peso) - 50
-multa = int(execesso) * 4
+#João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens adequadas.
+peso = int(input("Digite o peso dos peixes: "))
+execesso = peso - 50
+multa = execesso * 4
 
-if execesso < 1:
-    print("João não terá que pagar multa")
-else:
-    print("A multa que joão terá que pagar será de: ", multa)
+if execesso < 1: print("João não terá que pagar multa")
+else: print("A multa que joão terá que pagar será de: ", multa)
