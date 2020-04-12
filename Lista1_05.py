479fef42977f8a4e6c5840d44f8c89097f2b65f6
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:10:38 -0300

    Update Lista1_05.py

--- Lista1_05.py
+++ Lista1_05.py
@@ -1,4 +1,5 @@
-metros = input("Digite a quantidade de metros: ")
-centimetros = float(metros) * 100
+# Faça um Programa que converta metros para centímetros.
+metros = float(input("Digite a quantidade de metros: "))
+centimetros = metros * 100
 
 print(metros, "metros é igual a ", centimetros, "centimetros")
