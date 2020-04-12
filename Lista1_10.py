4f5419f6c9e4d6f57ae77cff27bc956d39097064
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:17:29 -0300

    Update Lista1_10.py

--- Lista1_10.py
+++ Lista1_10.py
@@ -1,4 +1,5 @@
-celsius = input("Quantidade de graus Celsius: ")
-fahrenheit = (float(celsius) * 1.8) + 32
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+celsius = float(input("Quantidade de graus Celsius: "))
+fahrenheit = (celsius * 1.8) + 32
 
 print(celsius, " graus Celsius são iguais a ", fahrenheit, " graus Fahrenheit")
