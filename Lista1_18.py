199dc91d5280cdabd42f78277d448c0b02482e4c
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-12 10:41:43 -0300

    Update Lista1_18.py

--- Lista1_18.py
+++ Lista1_18.py
@@ -1,9 +1,10 @@
-import math
+#Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+from math import ceil
 
-tamanho = input("Digite o tamanho do arquivo: ")
-velocidade = input("Digite a velocidade da internet: ")
+tamanho = float(input("Digite o tamanho do arquivo: "))
+velocidade = float(input("Digite a velocidade da internet: "))
 
-tempo = float(tamanho) / float(velocidade)
-minutos = math.ceil(float(tempo) / 60)
+tempo = tamanho / velocidade
+minutos = ceil(tempo) / 60)
 
 print("O arquivo levará ", minutos, "minutos para ficar pronto")
